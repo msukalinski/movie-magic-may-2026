2084c7e2dc3006bddb4a47a0cfea88530461ff0b
msukalinski <msukalinski@gmail.com>
2026-06-29 20:52:41 +0300

    render single movie on home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,9 +6,7 @@ const homeController = Router();
 homeController.get('/', async (req, res) => {
     const movies = await movieService.getAll();
 
-    console.log(movies);
-    
-    res.render('home');
+    res.render('home', { movie: movies[1] });
 });
 
 homeController.get('/about', (req, res) => {
