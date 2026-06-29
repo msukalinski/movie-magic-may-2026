54e283f103205c5bc88cf8d33d64a89890b8c3b8
msukalinski <msukalinski@gmail.com>
2026-06-29 21:34:58 +0300

    add body parser

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -4,6 +4,10 @@ const movieController = Router();
 
 movieController.get('/create', (req, res) => {
     res.render('movies/create');
+});
+
+movieController.post('/create', (req, res) => {
+    console.log(req.body);
 })
 
 export default movieController;
