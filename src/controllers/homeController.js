c2fcd1839641e7056e3c973d38b499db4c0151c4
msukalinski <msukalinski@gmail.com>
2026-06-29 20:48:13 +0300

    add movie service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { getAllMovies } from "../repositories/movieRepository.js";
+import movieService from "../services/movieService.js";
 
 const homeController = Router();
 
 homeController.get('/', async (req, res) => {
-    const movies = await getAllMovies();
+    const movies = await movieService.getAll();
 
     console.log(movies);
-
+    
     res.render('home');
 });
 
