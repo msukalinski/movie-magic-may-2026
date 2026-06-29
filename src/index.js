9b448b31cbb59c6d5fd6f24a956f44ee85b5c543
msukalinski <msukalinski@gmail.com>
2026-06-29 21:14:44 +0300

    add create movie page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import homeController from './controllers/homeController.js';
+import movieController from './controllers/movieController.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.static('./src/public'));
 
 //Setup routes
 app.use('/', homeController);
+app.use('/movies', movieController)
 
 //Start the server
 app.listen(5000, () => { console.log('Server is listening on: http://localhost:5000...') });
