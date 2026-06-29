fd8c452556cefd88c14bfc80f8f283b47f7bdc3e
msukalinski <msukalinski@gmail.com>
2026-06-29 20:35:56 +0300

    add movie repository

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import { getAllMovies } from "../repositories/movieRepository.js";
 
 const homeController = Router();
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
+    const movies = await getAllMovies();
+
+    console.log(movies);
+
     res.render('home');
 });
 
