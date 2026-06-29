0292c5f5798a8b835488d405f58b3fad33a93579
msukalinski <msukalinski@gmail.com>
2026-06-29 19:21:01 +0300

    add static middleware

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ app.engine('hbs', engine());
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+//Setup static assets
+app.use(express.static('./src/public'));
+
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
 });
+
 
 app.listen(5000, () => { console.log('Server is listening on: http://localhost:5000...') });
