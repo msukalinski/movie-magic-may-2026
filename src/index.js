72245773debc2d2132f0eb4bf4a282d4fb408f02
msukalinski <msukalinski@gmail.com>
2026-06-29 18:52:37 +0300

    add express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Neshto si..');
+});
+
+app.listen(5000, () => {console.log('Server is listening on: http://localhost:5000...')});
