fd181c02c4470c463c6b5cc1dad959c5c71b6aa2
msukalinski <msukalinski@gmail.com>
2026-07-05 20:17:17 +0300

    Filter movies db not in memory

--- README.md
+++ README.md
@@ -104,6 +104,6 @@ SoftUni JS Back-End Course Workshop
 
 ### Bonuses
  - [x] Show filtered artists in attach page
- - [] Search filter in db
+ - [x] Search filter in db
  - [] Name in movie (Explicit Many-to-Many)
  - [] Modify service export
--- src/repositories/movieRepository.js
+++ src/repositories/movieRepository.js
@@ -2,22 +2,22 @@ import fs from 'fs/promises';
 import { prisma } from '../lib/prisma.js'
 
 async function getAll(filter = {}) {
-    let movies = await prisma.movie.findMany();
+    let movies = await prisma.movie.findMany({
+        where: {
+            year: filter.year || undefined,
+            genre: {
+                equals: filter.genre || undefined,
+                mode: 'insensitive'
+            },
+            title: {
+                contains: filter.search,
+                mode: 'insensitive'
+            }
+        }
+    });
 
     //Partial case insensitive search
     //TODO Implement database filtering instead of filtering in memory
-    if (filter.search) {
-        movies = movies.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-    };
-
-    //Exact search
-    if (filter.year) {
-        movies = movies.filter(movie => movie.year === filter.year);
-    };
-
-    if (filter.genre) {
-        movies = movies.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
-    };
 
     return movies;
 };
