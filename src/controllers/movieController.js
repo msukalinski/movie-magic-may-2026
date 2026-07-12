74debed6d4d753b786b51f903c211b03cff5179f
msukalinski <msukalinski@gmail.com>
2026-07-12 18:54:42 +0300

    Show dynamic details buttons

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -30,14 +30,17 @@ movieController.post('/create', isAuth, async (req, res) => {
 //Details page
 movieController.get('/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
+    const userId = req?.user.id
 
     const movie = await movieService.getById(movieId);
+
+    const isOwner = movie.userId && movie.userId === userId;
 
     //Prepare view data | Quick and Dirty TODO: Fix it
     // const rating = ;
     const ratingStars = '&#x2605;'.repeat(Math.floor(movie.rating));
 
-    res.render('movies/details', { movie, pageTitle: 'Movie Details', ratingStars });
+    res.render('movies/details', { movie, pageTitle: 'Movie Details', ratingStars, isOwner });
 });
 
 movieController.get('/:movieId/attach', isAuth, async (req, res) => {
