ddb8188db9a33963b43d5f765978e0555694a246
msukalinski <msukalinski@gmail.com>
2026-07-12 20:26:57 +0300

    Modify select on edit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ const app = express();
 //Setup handlebars
 app.engine('hbs', engine({
     extname: 'hbs',
+    helpers: {
+        isSelected() {
+            return this.selected ?'selected' : '';
+        }
+    }
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
