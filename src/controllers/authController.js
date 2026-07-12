150730f3eed4d74730551d185c0bcdfbf8159b3d
msukalinski <msukalinski@gmail.com>
2026-07-12 16:26:15 +0300

    Return token as cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,7 +24,7 @@ authController.post('/login', async (req, res) => {
 
     const token = await authService.login({ email, password });
 
-    console.log(token);
+    res.cookie('auth', token, { httpOnly: true });
 
     res.redirect('/');
 });
