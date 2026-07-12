7d9d743ac97598e7d61578b5675b228743ba4379
msukalinski <msukalinski@gmail.com>
2026-07-12 15:28:39 +0300

    Hash password on user create

--- README.md
+++ README.md
@@ -116,10 +116,10 @@ SoftUni JS Back-End Course Workshop
 ### Register
  -[x] Add auth controller
  -[x] Add register page
- -[ ] Add register post action
- -[ ] Add service
- -[ ] Add repository
- -[ ] Add model
+ -[x] Add register post action
+ -[x] Add service
+ -[x] Add repository
+ -[x] Add model
  -[ ] Hash password `npm i bcrypt`
 
 ### Login
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,16 @@
+import bcrypt from 'bcrypt';
 
 import userRepository from "../repositories/userRepository.js";
 
+export async function create(userData) {
+    const hashPassword = await bcrypt.hash(userData.password, 10);
+    const result = await userRepository.create({
+        ...userData,
+        password: hashPassword
+    });
+
+    return result;
+};
 
 const authService = {
     create
