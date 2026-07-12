502fbd911e27445ba3ac225ca542fc6b19e472bd
msukalinski <msukalinski@gmail.com>
2026-07-12 20:08:18 +0300

    Use env for secret

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export function generateAuthToken(user) {
     const payload = { id: user.id, email: user.email };
-    const secret = 'SECRETGOESHERE';
+    const secret = process.env.AUTH_SECRET || 'default_secret';
     const token = jwt.sign(payload, secret, { expiresIn: '1h' });
 
     return token;
